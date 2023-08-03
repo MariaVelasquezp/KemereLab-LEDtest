68e45e3e741aa9adcbacb31f2a115bffff84ee6d
MariaVelasquezp <101753368+MariaVelasquezp@users.noreply.github.com>
2023-08-03 14:24:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,25 +11,42 @@
 */
 #include <project.h>
 
-int main(void)
-{
-    CyGlobalIntEnable; /* Enable global interrupts. */
+CY_ISR(isr_1_Handler) {
+    // Toggle the LED output
+    red_Write(!red_Read());
 
-    /* Start Sleep Timer with desired interval for flashing the LED */
-    CySysTickStart();
-
-    for (;;)
-    {
-        /* Flash the LED by toggling its state */
-        red_Write(1); /* Turn LED on */
-        CyDelay(100); /* Delay for ON state */
-        red_Write(0); /* Turn LED off */
-        CyDelay(900); /* Delay for OFF state */
-
-        /* Enter low power mode (Sleep) until Sleep Timer interrupt */
-        CySysPmSleep();
-    }
+    // Clear the Timer/Counter interrupt
+    Timer_ClearInterrupt(Timer_INTR_MASK_TC);
 }
 
+int main(void) {
+    CyGlobalIntEnable; /* Enable global interrupts. */
+
+    // Set the initial LED state (OFF)
+    red_Write(0);
+
+    // Attach the Timer/Counter ISR
+    isr_1_StartEx(isr_1_Handler);
+
+    for (;;) {
+        // Turn the LED on
+        red_Write(1);
+
+        // Enable the Timer/Counter
+        Timer_Start();
+
+        // Wait for the Timer/Counter to interrupt after 100 ms (0.1 second)
+        CyDelay(1);
+
+        // Disable the Timer/Counter
+        Timer_Stop();
+
+        // Turn the LED off
+        red_Write(0);
+
+        // Enter low power mode for 900 ms (0.9 seconds)
+        CyDelay(900);
+    }
+}
 
 /* [] END OF FILE */
