2f798b41f5c1cd5f8f7fc657764b369d199f2129
MariaVelasquezp <101753368+MariaVelasquezp@users.noreply.github.com>
2023-08-07 17:13:02 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,96 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include <project.h>
 
-CY_ISR(isr_1_Handler) {
-    // Toggle the LED output
-    red_Write(!red_Read());
+/******************************************************************************
+ * Macro definition
+ * ----------------------------------------------------------------------------
+ * These Macro are only used in this module for button pressing event 
+ * detection. These Macro should not be populated to other modules.
+ ******************************************************************************/
+#define LED_ON                (uint8)0x01
+#define LED_OFF               (uint8)0x00
+#define BLINK_INTERVAL_MS     500  /* Blink interval in milliseconds */
+#define ILO_FREQ              32000 /* Hz */
+#define DoSomething()         CyDelay(500) /* Delay for demonstration */
 
-    // Clear the Timer/Counter interrupt
-    Timer_ClearInterrupt(Timer_INTR_MASK_TC);
+/******************************************************************************
+ * Global variables definition
+ * ----------------------------------------------------------------------------
+ * These variables should be populated to other modules. The header file contains
+ * the extern statement for these variables.
+ ******************************************************************************/
+void InitWatchdog(uint16 sleep_interval);
+CY_ISR(isr_WDT);
+
+int main(void)
+{
+    /*===========================================================================================
+     * this code piece initializes the watchdog function 
+     *==========================================================================================*/
+    /* Initialize watchdog */
+    InitWatchdog(BLINK_INTERVAL_MS);
+    /* Connect ISR routine to Watchdog interrupt */
+    isr_WDT_StartEx(isr_WDT);
+    /* Set the highest priority to ensure ISR is executed in all conditions */
+    isr_WDT_SetPriority(0);
+    /* Enable global interrupts */
+    CyGlobalIntEnable;
+
+    for(;;)
+    {
+        /* Clear watchdog counter before deep sleep */
+        CySysWdtResetCounters(CY_SYS_WDT_COUNTER0_RESET);
+       
+        /* Go to deep sleep mode */
+        CySysPmDeepSleep();
+
+        /* Turn on Red LED after wakeup by Watchdog interrupt */
+        red_Write(LED_ON);        
+
+        /* Do something here after wakeup from deep sleep */
+        DoSomething();  
+
+        /* Turn off Red LED after doing something */
+        red_Write(LED_OFF);
+    }
 }
 
-int main(void) {
-    CyGlobalIntEnable; /* Enable global interrupts. */
+/*******************************************************************************
+* Function Name: InitWatchdog
+********************************************************************************
+* Summary:
+*  Initialize watchdog counter0 with the specific sleep interval parameter.
+*
+* Parameters:
+*  uint16 sleep_interval  Reset interval in millisecond unit.
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void InitWatchdog(uint16 sleep_interval)
+{
+    /*==============================================================================*/
+    /* Configure counter 0 for wakeup interrupt                   */
+    /*==============================================================================*/
+    /* Counter 0 of the Watchdog time generates periodically interrupt to wake up the system */
+    CySysWdtWriteMode(CY_SYS_WDT_COUNTER0, CY_SYS_WDT_MODE_INT);
+    /* Set interval as desired value */
+    CySysWdtWriteMatch(CY_SYS_WDT_COUNTER0, ((uint32)(sleep_interval * ILO_FREQ) / 1000));
+    /* Clear counter on match event */
+    CySysWdtWriteClearOnMatch(CY_SYS_WDT_COUNTER0, 1u);
+    /*==============================================================================*/
+    /* Enable watchdog                                                              */
+    /*==============================================================================*/
+    /* Enable the counter 0 */
+    CySysWdtEnable(CY_SYS_WDT_COUNTER0_MASK);
+    /* Check if counter 0 is enabled, otherwise keep looping here */
+    while (!CySysWdtReadEnabledStatus(CY_SYS_WDT_COUNTER0));
+}
 
-    // Set the initial LED state (OFF)
-    red_Write(0);
-
-    // Attach the Timer/Counter ISR
-    isr_1_StartEx(isr_1_Handler);
-
-    for (;;) {
-        // Turn the LED on
-        red_Write(1);
-
-        // Enable the Timer/Counter
-        Timer_Start();
-
-        // Wait for the Timer/Counter to interrupt after 100 ms (0.1 second)
-        CyDelay(1);
-
-        // Disable the Timer/Counter
-        Timer_Stop();
-
-        // Turn the LED off
-        red_Write(0);
-
-        // Enter low power mode for 900 ms (0.9 seconds)
-        CyDelay(900);
-    }
+CY_ISR(isr_WDT)
+{
+    /* Clear interrupt flag to enable the next interrupt */
+    CySysWdtClearInterrupt(CY_SYS_WDT_COUNTER0_INT);
 }
 
 /* [] END OF FILE */
