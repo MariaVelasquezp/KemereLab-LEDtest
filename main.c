d86e5561d62ebd5774338286c558fd01f1ebff01
MariaVelasquezp <101753368+MariaVelasquezp@users.noreply.github.com>
2023-08-08 00:42:01 -0500

    main.c

--- main.c
+++ main.c
@@ -1,96 +1,69 @@
-#include <project.h>
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#include "project.h"
+#include <stdbool.h>
+bool state = true;
 
-/******************************************************************************
- * Macro definition
- * ----------------------------------------------------------------------------
- * These Macro are only used in this module for button pressing event 
- * detection. These Macro should not be populated to other modules.
- ******************************************************************************/
-#define LED_ON                (uint8)0x01
-#define LED_OFF               (uint8)0x00
-#define BLINK_INTERVAL_MS     500  /* Blink interval in milliseconds */
-#define ILO_FREQ              32000 /* Hz */
-#define DoSomething()         CyDelay(500) /* Delay for demonstration */
-
-/******************************************************************************
- * Global variables definition
- * ----------------------------------------------------------------------------
- * These variables should be populated to other modules. The header file contains
- * the extern statement for these variables.
- ******************************************************************************/
-void InitWatchdog(uint16 sleep_interval);
-CY_ISR(isr_WDT);
+void interrupcion_malu(){
+    state=!state;
+    /*
+    if(state){
+        state=false;
+        red_Write(0);
+        red_2_Write(0);
+    }
+    else {
+     state= true;   
+         red_Write(1);
+        red_2_Write(1);
+    }*/
+    
+    uint32 reason = CySysWdtGetInterruptSource();  
+    CySysWdtClearInterrupt(reason); 
+}
 
 int main(void)
 {
-    /*===========================================================================================
-     * this code piece initializes the watchdog function 
-     *==========================================================================================*/
-    /* Initialize watchdog */
-    InitWatchdog(BLINK_INTERVAL_MS);
-    /* Connect ISR routine to Watchdog interrupt */
-    isr_WDT_StartEx(isr_WDT);
-    /* Set the highest priority to ensure ISR is executed in all conditions */
-    isr_WDT_SetPriority(0);
-    /* Enable global interrupts */
-    CyGlobalIntEnable;
+    //WDT2 definido en user design
+    //User Provided implica dar dirección de función a la ISR
+    isr_1_StartEx(interrupcion_malu);
+    
+    CySysWdtUnlock(); // Enable modification of the WDT Timers
+    
+    // Turn off the WDTs (all three of them)
+    CySysWdtDisable(CY_SYS_WDT_COUNTER0_MASK); 
+    CySysWdtDisable(CY_SYS_WDT_COUNTER1_MASK);
+    CySysWdtDisable(CY_SYS_WDT_COUNTER2_MASK);
+    
+    // Make Timer 0 & 1 run with no interrupt, and 2 cause an interrupt
+    CySysWdtSetMode(CY_SYS_WDT_COUNTER0,CY_SYS_WDT_MODE_NONE);
+    CySysWdtSetMode(CY_SYS_WDT_COUNTER1,CY_SYS_WDT_MODE_NONE);
+    CySysWdtSetMode(CY_SYS_WDT_COUNTER2,CY_SYS_WDT_MODE_INT);
+    
+    // Set the time to 32768/(64*64*2^6) = 32768/(2^18) = 0.125 = 1/8 Hz
+    CySysWdtSetMatch(CY_SYS_WDT_COUNTER0, 16);
+    CySysWdtSetMatch(CY_SYS_WDT_COUNTER1, 16);
+    CySysWdtSetToggleBit(6);
+    
+    CySysWdtSetClearOnMatch(CY_SYS_WDT_COUNTER0,1); // When counter his period reset counter
+    CySysWdtSetClearOnMatch(CY_SYS_WDT_COUNTER1,1);
 
-    for(;;)
-    {
-        /* Clear watchdog counter before deep sleep */
-        CySysWdtResetCounters(CY_SYS_WDT_COUNTER0_RESET);
-       
-        /* Go to deep sleep mode */
-        CySysPmDeepSleep();
-
-        /* Turn on Red LED after wakeup by Watchdog interrupt */
-        red_Write(LED_ON);        
-
-        /* Do something here after wakeup from deep sleep */
-        DoSomething();  
-
-        /* Turn off Red LED after doing something */
-        red_Write(LED_OFF);
+    CySysWdtSetCascade(CY_SYS_WDT_CASCADE_01 | CY_SYS_WDT_CASCADE_12); // Cascade 0-1-2
+    CySysWdtEnable(CY_SYS_WDT_COUNTER0_MASK | CY_SYS_WDT_COUNTER1_MASK | CY_SYS_WDT_COUNTER2_MASK );
+    
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    
+    for(;;) {
+    CySysPmDeepSleep(); // wait for interrupt in deep sleep    
     }
 }
-
-/*******************************************************************************
-* Function Name: InitWatchdog
-********************************************************************************
-* Summary:
-*  Initialize watchdog counter0 with the specific sleep interval parameter.
-*
-* Parameters:
-*  uint16 sleep_interval  Reset interval in millisecond unit.
-*
-* Return:
-*  void
-*
-*******************************************************************************/
-void InitWatchdog(uint16 sleep_interval)
-{
-    /*==============================================================================*/
-    /* Configure counter 0 for wakeup interrupt                   */
-    /*==============================================================================*/
-    /* Counter 0 of the Watchdog time generates periodically interrupt to wake up the system */
-    CySysWdtWriteMode(CY_SYS_WDT_COUNTER0, CY_SYS_WDT_MODE_INT);
-    /* Set interval as desired value */
-    CySysWdtWriteMatch(CY_SYS_WDT_COUNTER0, ((uint32)(sleep_interval * ILO_FREQ) / 1000));
-    /* Clear counter on match event */
-    CySysWdtWriteClearOnMatch(CY_SYS_WDT_COUNTER0, 1u);
-    /*==============================================================================*/
-    /* Enable watchdog                                                              */
-    /*==============================================================================*/
-    /* Enable the counter 0 */
-    CySysWdtEnable(CY_SYS_WDT_COUNTER0_MASK);
-    /* Check if counter 0 is enabled, otherwise keep looping here */
-    while (!CySysWdtReadEnabledStatus(CY_SYS_WDT_COUNTER0));
-}
-
-CY_ISR(isr_WDT)
-{
-    /* Clear interrupt flag to enable the next interrupt */
-    CySysWdtClearInterrupt(CY_SYS_WDT_COUNTER0_INT);
-}
-
 /* [] END OF FILE */
